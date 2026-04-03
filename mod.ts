31ffc591bebd8fe2a9400f3f82561746fc0a9572
Asher Gomez <ashersaupingomez@gmail.com>
2026-04-03 21:14:34 +1100

    feat: `assertWebhookRequest()`

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 import createClient from "openapi-fetch";
-import type { paths } from "./types.d.ts";
+import { timingSafeEqual } from "@std/crypto/timing-safe-equal";
+import type { components, paths } from "./types.d.ts";
 
 export * from "./types.d.ts";
 
@@ -19,9 +20,7 @@ const BASE_URL = "https://api.getcircuit.com/public/v0.2b";
  * import { createSpokeClient } from "@incapost/spoke";
  *
  * const spokeClient = createSpokeClient("your_spoke_api_key");
- *
  * const { data } = await spokeClient.GET("/plans");
- *
  * // ...
  * ```
  *
@@ -41,4 +40,70 @@ export function createSpokeClient(
     },
   });
   return spokeClient;
+}
+
+// Until Spoke publishes this definition
+export interface WebhookRequestBody {
+  type:
+    | "stop.allocated"
+    | "stop.out_for_delivery"
+    | "stop.attempted_delivery"
+    | "test.send_event";
+  version: "v0.2b";
+  created: number;
+  data: components["schemas"]["stopSchema"];
+}
+
+/**
+ * Asserts that a webhook request is valid by verifying its signature using the
+ * provided webhook secret key.
+ *
+ * @throws {TypeError} If the signature header is missing or if the signature is
+ * incorrect.
+ *
+ * @param webhookSecretKey The webhook secret key used to verify the signature
+ * of the request. Generate it at
+ * {@link https://dispatch.spoke.com/settings/integrations}.
+ * @param request The incoming webhook request to be verified.
+ *
+ * @see {@link https://developer.dispatch.spoke.com/docs/getting-started/securing-the-endpoint | Securing the Endpoint}
+ *
+ * @example Usage
+ * ```ts
+ * import { assertWebhookRequest, type WebhookRequestBody } from "@incapost/spoke";
+ *
+ * async function handleWebhook(request: Request) {
+ *   try {
+ *     await assertWebhookRequest("your_webhook_secret_key", request);
+ *     const body = (await request.json()) as WebhookRequestBody;
+ *     // Process the webhook request
+ *   } catch (error) {
+ *     return new Response("Invalid signature", { status: 400 });
+ *   }
+ * }
+ * ```
+ */
+export async function assertWebhookRequest(
+  webhookSecretKey: string,
+  request: Request,
+) {
+  const signature = request.headers.get("spoke-signature");
+  if (!signature) {
+    throw new TypeError("Missing signature header");
+  }
+
+  const key = await crypto.subtle.importKey(
+    "raw",
+    new TextEncoder().encode(webhookSecretKey),
+    { name: "HMAC", hash: "SHA-256" },
+    false,
+    ["sign"],
+  );
+
+  const body = await request.clone().arrayBuffer();
+  const expected = await crypto.subtle.sign("HMAC", key, body);
+  const received = Uint8Array.fromHex(signature);
+  if (!timingSafeEqual(expected, received)) {
+    throw new TypeError("Incorrect signature");
+  }
 }
